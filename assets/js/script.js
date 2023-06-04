34dc48b6c3a27a665bffd834dd7beaf211ada59c
Samuel Liew <samliew@gmail.com>
2023-06-04 14:20:44 +0800

    Refactor JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
+/* ===== HELPER FUNCTIONS ===== */
+
+// scroll to element
+const animateTo = function (scrollY) {
+  if ($ && $.fn && $.fn.animate) {
+    $('html, body').stop(1).animate({
+      scrollTop: scrollY
+    }, 400);
+  }
+};
+// highlight and go to event
+const goToEvent = function (slug) {
+  const elem = document.querySelector('#' + slug);
+  if (!elem) return;
+  elem.click();
+  elem.classList.add('highlight');
+  setTimeout(elem => {
+    elem.classList.remove('highlight');
+  }, 2000, elem);
+};
+// go to year
+const goToYear = function (year) {
+  year = Number(year);
+  if (!isNaN(year)) $(`.timeline-year[data-year="${year}"]`).click();
+};
+// string to tags
+const tagsStrToClass = function (str) {
+  return str.toLowerCase().trim().split(/\s+/g)
+    .map(v => 'tag-' + v.replace(/(^\W+|\W+$)/g, '').replace(/\W+/g, '-'))
+    .join(' ');
+};
+
+
+
+/* ===== TIMELINE INIT/BUILDER FUNCTIONS ===== */
 const createYearGroupElem = year => {
   // Validation
   const y = Number(year);
@@ -72,7 +107,7 @@ const createEventElem = event => {
   return eventEl;
 };
 
-$(async function () {
+const buildTimeline = async () => {
 
   // Build timeline from json
   let currentYear = 0, currentYearEventsElem;
@@ -103,81 +138,28 @@ $(async function () {
   const beginning = [...timelineEl.querySelectorAll('.static-year')].pop();
   timelineEl.append(beginning);
 
-  // Helper: remove highlight
-  const removeHighlight = function () {
-    $('.event.highlight').removeClass('highlight');
-  };
-  // Helper: scroll to element
-  const animateTo = function (scrollY) {
-    $('html, body').stop(1).animate({
-      scrollTop: scrollY
-    }, 400);
-  };
-  // Helper: go to event
-  const goToEvent = function (slug) {
-    const elem = $('#' + slug).click();
-    elem.addClass('highlight');
-    setTimeout(removeHighlight, 2000);
-  };
-  // Helper: go to year
-  const goToYear = function (year) {
-    year = Number(year);
-    if (!isNaN(year)) $(`.timeline-year[data-year="${year}"]`).click();
-  };
-  // Helper: string to tags
-  const tagsStrToClass = function (str) {
-    return str.toLowerCase().trim().split(/\s+/g)
-      .map(v => 'tag-' + v.replace(/(^\W+|\W+$)/g, '').replace(/\W+/g, '-'))
-      .join(' ');
-  };
-
-  const events = $('.event');
-
-  // Remove unused elements
-  $('.event-summary, .event-description, .event-link', events)
-    .filter((i, el) => el.textContent == '').remove();
+  // Remove unused/empty elements
+  document.querySelectorAll('.event-summary, .event-description, .event-link').forEach(el => {
+    if (el.textContent?.trim() === '') el.remove();
+  });
 
   // Resize small and large boxes
-  $('.box').each(function () {
-    var h = $(this).outerHeight();
-    if (h < 180) $(this).parent().addClass('event-small');
-    if (h > 260) $(this).parent().addClass('event-large');
+  document.querySelectorAll('.box').forEach(el => {
+    const $el = $(el);
+    var h = $el.outerHeight();
+    if (h < 180) $el.parent().addClass('event-small');
+    if (h > 260) $el.parent().addClass('event-large');
   });
 
-  // To turn autoscroll on, replace click with mouseover
-  const topHeight = $('header').outerHeight() || 80;
-  const offsetConstant = 108 + topHeight;
-  $('.timeline').on('click', '.event', function () {
-    history.replaceState(null, document.title, `${location.pathname}${location.search}#${this.id}`);
-    animateTo($(this).offset().top - offsetConstant);
-  }).on('click', '.timeline-year:not(.static-year)', function () {
-    animateTo($(this).next().children().first().offset().top - offsetConstant);
-  }).on('click', '.linked-event', function (evt) {
-    evt.stopPropagation();
-    goToEvent(this.hash.substr(1));
-    return false;
+  // Show url in tooltips for links that don't have a title
+  document.querySelectorAll('.event a:not([title])').forEach(el => {
+    el.title = el.href;
   });
+};
 
-  // Apply fake permalinks (dynamic link anchors) to events
-  let scrollToPermalink = () => { };
-  $('.event-name').each(function (i, el) {
-    const slug = el.textContent.toLowerCase().replace(/[^\w\s]+/g, '').replace(/\s+/g, '-').trim();
-    const evt = $(this).parents('.event').attr('id', (i, v) => v ? v : slug);
+const initYearNav = () => {
 
-    // On page load scroll to item if match hash
-    if (slug === location.hash.substr(1)) {
-      scrollToPermalink = function () {
-        setTimeout(function () {
-          animateTo(evt.offset().top - offsetConstant);
-          evt.addClass('highlight');
-          setTimeout(removeHighlight, 2000);
-        }, 200);
-      };
-      scrollToPermalink();
-    }
-  });
-
-  // Year selector and sidebar nav
+  // Add year selector and sidebar nav
   const yearPicker = $('#year-picker').empty().on('input', function (i, el) {
     goToYear(this.value);
   }).on('click', 'option', function (i, el) {
@@ -217,7 +199,8 @@ $(async function () {
   };
   $(document).on('load scroll', $.debounce(50, scrollUpdateCurrentYear));
 
-  // Show sidebar when mouse near right hand side
+
+  // Show desktop sidebar when mouse near right hand side
   const mouseNearby = Math.min(180, document.body.clientWidth * 0.15); // pixels within this area next to sidebar
   let sidebarTimeout;
   const mousemoveUpdateSidebar = function (event) {
@@ -233,9 +216,52 @@ $(async function () {
     }
   };
   $(document).on('mousemove', $.debounce(10, e => mousemoveUpdateSidebar(e)));
+};
 
-  const tags = $('.event-tags');
+
+
+/* ===== MAIN FUNCTION ===== */
+$(async function () {
+  await buildTimeline();
+  initYearNav();
+
+  // When event box is clicked, scroll to it
+  const topHeight = $('header').outerHeight() || 80;
+  const offsetConstant = 108 + topHeight;
+  $('.timeline').on('click', '.event', function () {
+    history.replaceState(null, document.title, `${location.pathname}${location.search}#${this.id}`);
+    animateTo($(this).offset().top - offsetConstant);
+  }).on('click', '.timeline-year:not(.static-year)', function () {
+    animateTo($(this).next().children().first().offset().top - offsetConstant);
+  }).on('click', '.linked-event', function (evt) {
+    evt.stopPropagation();
+    goToEvent(this.hash.substr(1));
+    return false;
+  });
+
+
+  // Apply id slugs to events that does not have one
+  $('.event-name').each(function (i, el) {
+    const slug = el.textContent.toLowerCase().replace(/[^\w\s]+/g, '').replace(/\s+/g, '-').trim();
+    const eventEl = $(this).parents('.event').attr('id', (i, v) => v ? v : slug);
+
+    // On page load scroll to item if match hash
+    if (slug === location.hash?.slice(1)) {
+      const scrollToPermalink = function () {
+        animateTo(eventEl.offset().top - offsetConstant);
+        eventEl.addClass('highlight');
+        setTimeout(elem => {
+          elem.classList.remove('highlight');
+        }, 2000, eventEl);
+      };
+      setTimeout(scrollToPermalink, 100);
+    }
+  });
+
+
   // Pre-process - set tags as a class for better filter toggling
+  const events = $('.event');
+  const tags = $('.event-tags');
   tags.each(function (i, el) {
     $(this).closest('.event').addClass(tagsStrToClass(el.textContent));
   });
@@ -261,15 +287,11 @@ $(async function () {
       .toggleClass('event-hidden', chk);
   });
 
+
   // Back to top
   $('#back-to-top').click(function () {
     animateTo(0);
     return false;
-  });
-
-  // Tooltips for discuss links
-  $('.event a:not([title])').each(function () {
-    this.title = this.href;
   });
 
 });
